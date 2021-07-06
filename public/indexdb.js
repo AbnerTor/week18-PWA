bd98db8b7c5c01dfee72fa59bb0cd6df19e069fc
ABNER TORIBIO <abnertoribio@live.com>
2021-07-06 19:06:53 -0400

    editing structure

--- public/indexdb.js
+++ public/indexdb.js
@@ -1,12 +1,16 @@
 const { response } = require("express");
 
 let db;
+
 const request = indexedDB.open('transactions', 1);
 
 request.onupgradeneeded = function (e) {
     db = e.target.result;
 
+    if(db.ObjectStoreNames.length === 0) {
     db.createObjectStore('transactions', { autoIncrement: true});
+
+    }
 
 };
 
@@ -16,6 +20,13 @@ request.onsuccess = function(e) {
         checkDatabase();
     }
 };
+
+const saveInput = (input) => {
+    console.log('Saving to IndexDB');
+
+    const transaction = db.transaction(['transactions'], 'readwrite');
+    transaction.ObjectStore('transactions').add(input);
+}
 
 function checkDatabase() {
     let transaction = db.transaction(['transactions'], 'readwrite');
